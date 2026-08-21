f35e0a00029671614e25c51acce00cebab7eebe7
Erick Díaz <erickdiazc717@gmail.com>
2026-08-20 20:07:52 -0500

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/erickdc/lacasadelalfajorbackend/LaCasaDelAlfajorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.erickdc.lacasadelalfajorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaCasaDelAlfajorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaCasaDelAlfajorBackendApplication.class, args);
+    }
+
+}
